d5884f9e750769e64314315d084ecb38a0d1de40
shamimasen <shalyalionita@gmail.com>
2024-09-11 14:43:02 +0700

    PWS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://shalya-naura-mentalhealthtracker2.pbp.cs.ui.ac.id/>", "http://shalya-naura-mentalhealthtracker2.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://shalya-naura-mentalhealthtracker2.pbp.cs.ui.ac.id/", "http://shalya-naura-mentalhealthtracker2.pbp.cs.ui.ac.id/"]
