c2af8c187761107846c5802198fc3f62bad00db7
shamimasen <shalyalionita@gmail.com>
2024-11-14 19:11:32 +0700

    tutorial 8

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from main.views import login_user
 from main.views import logout_user
 from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, add_mood_entry_ajax
 from main.views import edit_mood
-from main.views import delete_mood
+from main.views import delete_mood, create_mood_flutter
 
 app_name = 'main'
 
@@ -21,5 +21,5 @@ urlpatterns = [
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'),
     path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
-
+    path('create-flutter/', create_mood_flutter, name='create_mood_flutter'),
 ]
